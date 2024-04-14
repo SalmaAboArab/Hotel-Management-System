f457d529db33d8ab786c38a5b841bb3635b70192
Lames-Atef <lameatef53@gmail.com>
2024-04-14 18:51:34 +0200

    sidebarAndChange

--- src/AdminModule/Components/Sidebar/Sidebar.tsx
+++ src/AdminModule/Components/Sidebar/Sidebar.tsx
@@ -1,7 +1,312 @@
-import React from 'react'
+import * as React from 'react';
+import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import PersonIcon from '@mui/icons-material/Person';
+import AddHomeIcon from '@mui/icons-material/AddHome';
+import MuiDrawer from '@mui/material/Drawer';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import List from '@mui/material/List';
+import AssignmentReturnIcon from '@mui/icons-material/AssignmentReturn';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+import IconButton from '@mui/material/IconButton';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import Modal from 'react-bootstrap/Modal';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import BeenhereIcon from '@mui/icons-material/Beenhere';
+import ListItemText from '@mui/material/ListItemText';
+import AppsIcon from '@mui/icons-material/Apps';
+import LockIcon from '@mui/icons-material/Lock';
+import { useNavigate } from 'react-router-dom';
+import ChangePassword from '../../../AuthModule/Components/ChangePassword/ChangePassword';
+import { Link } from 'react-router-dom';
+
+import { InsertLink } from '@mui/icons-material';
+const drawerWidth = 210;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  backgroundColor:"#203FC7",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+
+
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
+
 
 export default function Sidebar() {
+
+  const navigateTo=useNavigate();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleToggle = () => {
+    setOpen(!open);
+  };
+ 
+  const [show, setShow] = React.useState(false);
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+ 
   return (
-    <div>Sidebar</div>
-  )
+    <>
+   < Modal show={show} onHide={handleClose}>
+        <Modal.Body>
+        <ChangePassword handleClose={handleClose}/>
+        </Modal.Body>
+      </Modal>
+
+  
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <Drawer variant="permanent" open={open} >
+        <DrawerHeader>
+      
+          <IconButton onClick={handleToggle}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List sx={{backgroundColor:"#203FC7",color:"white"}}>
+
+            <ListItem  disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+              onClick={()=>{navigateTo("/Admin/home")}}
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    color:"white",
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                <AddHomeIcon/> 
+                </ListItemIcon>
+                <ListItemText primary="Home"  sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          
+            <ListItem  disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+              onClick={()=>{navigateTo("/Admin/users")}}
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              > 
+                <ListItemIcon
+                  sx={{
+                    color:"white",
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                 <PersonIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Users"  sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+
+            <ListItem  disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                onClick={()=>{navigateTo("/Admin/rooms")}}
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    color:"white",
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                 <AppsIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Rooms"  sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+
+            <ListItem  disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    color:"white",
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                 <CalendarMonthIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Ads"  sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+
+            <ListItem  disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                onClick={()=>{navigateTo("/Admin/booking-list")}}
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    color:"white",
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+               <BeenhereIcon/>  
+                </ListItemIcon>
+                <ListItemText primary="Bookings"  sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+
+            <ListItem  disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+              onClick={()=>{navigateTo("/Admin/facilities")}}
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    color:"white",
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                 <AutoAwesomeIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Facilities"  sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+
+            <ListItem  disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+           
+           onClick= {handleShow} 
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+   
+                <ListItemIcon
+                  sx={{
+                    color:"white",
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                    
+                 <LockIcon/>
+                </ListItemIcon>
+                <ListItemText primary="ChangePassword"  sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+
+           
+            <ListItem  disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+              onClick={()=>{navigateTo("/")}}
+                sx={{
+                  color:"white",
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    color:"white",
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                 <AssignmentReturnIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Logout"  sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+        </List>
+       
+      </Drawer>
+      
+    </Box>
+    </>
+  );
 }
--- src/AuthModule/Components/ChangePassword/ChangePassword.tsx
+++ src/AuthModule/Components/ChangePassword/ChangePassword.tsx
@@ -1,7 +1,146 @@
-import React from 'react'
+import { Button, Dialog, DialogContent,  Stack, TextField, Typography } from '@mui/material'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
+import { baseUrl } from '../../../Constants/Components/Urls';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
-export default function ChangePassword() {
+export default function ChangePassword({handleClose}) {
+  
+    const [open, setOpen] = useState(false);
+    
+    const [showPasswordOldPassword, setShowPasswordOldPassword] = useState(true);
+  const [showPasswordNewPassword, setShowPasswordNewPassword] = useState(true);
+  const [showPasswordConfirmPassword, setShowPasswordConfirmPassword] =useState(true);
+
+  const togglePasswordVisibilityOldPassword = () => {
+    setShowPasswordOldPassword(!showPasswordOldPassword);
+  };
+
+  const togglePasswordVisibilityNewPassword = () => {
+    setShowPasswordNewPassword(!showPasswordNewPassword);
+  };
+
+  const togglePasswordVisibilityConfirmPassword = () => {
+    setShowPasswordConfirmPassword(!showPasswordConfirmPassword);
+  };
+   
+
+    const {register,handleSubmit,watch, formState: { errors },setValue}=useForm();
+
+const submitData=async(data:IChangePassword)=>{
+try {
+  const response=await axios.post(`${baseUrl}/admin/users/change-password`,data,
+  {headers:{Authorization:"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjFhYTZkNzZlYmJiZWZiYzE5ZjMyM2UiLCJyb2xlIjoiYWRtaW4iLCJ2ZXJpZmllZCI6ZmFsc2UsImlhdCI6MTcxMzAyNDQ1MCwiZXhwIjoxNzE0MjM0MDUwfQ.WGP2BzDpvgwhKVCenvoDk7yOr4GnG4utAhSnf-baTOg"}})
+  console.log(response)
+} catch (error) {
+  
+}
+}
+
+
   return (
-    <div>ChangePassword</div>
+    <div>
+      <div className='text-center'>
+        <h2>Change Password</h2>
+      </div>
+         
+          <form  onSubmit={handleSubmit(submitData)}>
+      <Stack spacing={2} margin={2}>
+
+      <div className='d-flex form-outline  position-relative'>
+   <TextField
+   sx={{width:"100%"}}
+   type={!showPasswordOldPassword ? "text" : "password"}
+   {...register("oldPassword")}
+       variant="filled"
+       size="small"
+        label="old Password"
+        name="oldPassword"
+        required
+      />
+                <ListItemIcon
+                onClick={togglePasswordVisibilityOldPassword}
+                  sx={{
+                    color:"blue",
+                    position:"absolute",
+                    minWidth: 0,
+                    right:0,
+                    paddingTop:2,
+                    mr: open ? 2 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                   {showPasswordOldPassword ?<RemoveRedEyeIcon/> :<VisibilityOffIcon/>}
+                </ListItemIcon>
+                
+   </div>
+   
+   <div className='d-flex form-outline  position-relative'>
+   <TextField
+   sx={{width:"100%"}}
+   type={!showPasswordNewPassword ? "text" : "password"}
+   {...register("newPassword")}
+       variant="filled"
+       size="small"
+        label="New Password"
+        name="newPassword"
+        required
+      />
+                <ListItemIcon
+                onClick={togglePasswordVisibilityNewPassword}
+                  sx={{
+                    color:"blue",
+                    position:"absolute",
+                    minWidth: 0,
+                    right:0,
+                    paddingTop:2,
+                    mr: open ? 2 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                   {showPasswordNewPassword ?<RemoveRedEyeIcon/> :<VisibilityOffIcon/>}
+                </ListItemIcon>
+   </div>
+
+
+<div className='d-flex form-outline  position-relative'>
+<TextField
+ sx={{width:"100%"}}
+      variant="filled"
+      size="small"
+        label="Confirm Password"
+       
+        type={!showPasswordConfirmPassword ? "text" : "password"}
+        {...register("confirmPassword")}
+        required
+        name="confirmPassword"
+      />
+       <ListItemIcon
+                onClick={togglePasswordVisibilityConfirmPassword}
+                  sx={{
+                    color:"blue",
+                    position:"absolute",
+                    minWidth: 0,
+                    right:0,
+                    paddingTop:2,
+                    mr: open ? 2 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                   {showPasswordConfirmPassword ?<RemoveRedEyeIcon/> :<VisibilityOffIcon/>}
+                </ListItemIcon>
+</div>
+
+      <Button type="submit" variant="contained" color="primary">
+        Submit
+      </Button>
+      </Stack>
+    </form>
+     
+    </div>
   )
 }
