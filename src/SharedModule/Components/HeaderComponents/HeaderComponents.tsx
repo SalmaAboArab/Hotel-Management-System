9eb5288ae64bd744fe680248ad96cae8323e41aa
SalmaAboArab <116819016+SalmaAboArab@users.noreply.github.com>
2024-04-18 03:19:55 +0200

    Revert "Ads component"

--- src/SharedModule/Components/HeaderComponents/HeaderComponents.tsx
+++ src/SharedModule/Components/HeaderComponents/HeaderComponents.tsx
@@ -1,47 +1,43 @@
 import { Box, Button, Typography, createTheme } from "@mui/material";
 
-type HeaderProps = {
-  title: string;
-  buttonName: string;
-  addOn: 'yes' | 'no'; 
-  anyFunction: () => void; 
+type props={
+    title:string;
+    buttonName:string;
+    addOn:string;
 }
-export default function HeaderComponents({
-  title,
-  buttonName,
-  addOn,
-  anyFunction
-}: HeaderProps) {
-  const theme = createTheme({
-    components: {
-      MuiButton: {
-        styleOverrides: {
-          root: {
+export default function HeaderComponents({ title, buttonName ,addOn }:props) {
+    const theme = createTheme({
+        button: {
             fontSize: 16,
             fontWeight: 600
-          }
-        }
-      }
-    }
-  });
+        },
+    });
 
-  return (
-    <Box sx={{ textTransform: "capitalize", padding: 2, my: 4, display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-      <Box>
-        <Typography component="h2" variant="h4">
-          {title}
-        </Typography>
-        <Typography component="p" variant="h6">
-          You can check all details
-        </Typography>
-      </Box>
-      {addOn === "no" ? null : (
-        <Box>
-          <Button onClick={anyFunction} type="submit" sx={{ paddingX: 3, textTransform: "capitalize", fontSize: theme.components?.MuiButton.styleOverrides?.root?.fontSize, fontWeight: theme.components?.MuiButton.styleOverrides?.root?.fontWeight }} variant="contained">
-            {buttonName}
-          </Button>
-        </Box>
-      )}
-    </Box>
-  );
+    return (
+        <>
+            <Box sx={{ textTransform: "capitalize", padding: 2, my:4, display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+
+
+                <Box>
+
+                    <Typography component={"h2"} variant="h4">
+                        {title}
+                    </Typography>
+                    <Typography component={"p"} variant="h6">
+                        you can check all details
+                    </Typography>
+                </Box>
+
+               {addOn=='no'?'':
+                <Box>
+
+                <Button type="submit" sx={{ paddingX: 3, textTransform: "capitalize", fontSize: theme.button.fontSize, fontWeight: theme.button.fontWeight  }} variant="contained">{buttonName}</Button>
+                
+                </Box>
+               }
+
+
+            </Box>
+        </>
+    );
 }
