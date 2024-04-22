dbcce2a2c478029a7fdf1151b94c5250fdc08cdc
3bdalaEhab <abdalaehab3@gmail.com>
2024-04-23 01:06:43 +0200

    footer

--- src/SharedModule/Components/MasterLayout/MasterLayout.tsx
+++ src/SharedModule/Components/MasterLayout/MasterLayout.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
 import Navbar from '../../../UsersModule/Components/Navbar/Navbar'
 import Footer from '../../../UsersModule/Components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
+import { Box } from '@mui/material'
 
 export default function MasterLayout() {
   return (
     <div>
         <Navbar/>
+        <Box sx={{height:"100vh"}}>
+
         <Outlet/>
+        </Box>
         <Footer/>
     </div>
   )
