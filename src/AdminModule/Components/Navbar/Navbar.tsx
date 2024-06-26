5ab194ec633198c434332114973d631659ca8ba6
3bdalaEhab <abdalaehab3@gmail.com>
2024-06-26 23:51:40 +0300

    Work navbar responsive

--- src/AdminModule/Components/Navbar/Navbar.tsx
+++ src/AdminModule/Components/Navbar/Navbar.tsx
@@ -2,124 +2,91 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
-import Button from '@mui/material/Button';
-import React, { useEffect, useState } from "react";
-import { baseUrl } from '../../../Constants/Components/Urls';
+import Button from "@mui/material/Button";
+import  { useEffect, useState } from "react";
+import { baseUrl } from "../../../Constants/Components/Urls";
 import axios from "axios";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { Avatar } from "@mui/material";
-import defaultImage from '../../../assets/Avatar.png'
-
-
-
+import { Avatar, Grid } from "@mui/material";
+import defaultImage from "../../../assets/Avatar.png";
 
 export default function Navbar() {
-  const[userData,setUserData]=useState();
-//let loginData=localStorage.getItem('loginData');
-const loginData=JSON.parse(localStorage.getItem('loginData'))
-//console.log(loginData._id);
-let token = localStorage.getItem("adminToken");
-async function getUserDetails() {
-  try {
-    const response= await axios.get(`${baseUrl}/portal/users/${loginData._id}`, {
-      headers:
-         { Authorization: token },
-    });
+  const [userData, setUserData] = useState();
+  const loginData = JSON.parse(localStorage.getItem("loginData"));
+  let token = localStorage.getItem("adminToken");
+  async function getUserDetails() {
+    try {
+      const response = await axios.get(
+        `${baseUrl}/portal/users/${loginData._id}`,
+        {
+          headers: { Authorization: token },
+        }
+      );
 
-    setUserData(response.data.data.user);
-    console.log(response.data.data.user);
-    
-  } catch (error) {
-    console.error(error);
+      setUserData(response.data.data.user);
+      console.log(response.data.data.user);
+    } catch (error) {
+      console.error(error);
+    }
   }
-  
-}
-useEffect(() => {
-  getUserDetails();
-}, []);
-
-
-  
+  useEffect(() => {
+    getUserDetails();
+  }, []);
 
   return (
-   <>
-
-<Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar sx={{background:'#FFFFFF'}}>
-        <Typography
-           sx={{flexGrow:1}}
-            variant="h4"
-            component="div"
-            color="initial"
-          >
-            <Typography
-              variant=""
-             
-              style={{ fontSize: "clamp(1rem, 4vw, 2rem)",color:'#3d5afe', marginLeft:'70px' }}
-            >
-              Stay
-            </Typography>
-            cation.
-          </Typography>
-         <Avatar   src={
-                      userData?.profileImage == null
-                        ? defaultImage
-                        : 
-                        userData?.profileImage
-                    }
-                  />
-          <Typography variant="caption" color="black" sx={{padding:'15px'}}>
-                {userData?.userName}
-              </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
-{/*<AppBar position="static" >
-        <Container maxWidth="xl" sx={{background:'#FFFFFF'}}>
-          <Toolbar disableGutters>
-            <Box sx={{ flexGrow: 1, display: { sm: "none" }, color: "black" }}>
-              <IconButton
-               
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Grid container sx={{ background: "#FFFFFF" }} p={1} px={3}>
+            <Grid item xs={12} sm={6}>
+              <Typography
+                display={"flex"}
+                alignItems={"center"}
+                justifyContent={{ xs: "center", sm: "start" }}
+                sx={{ flexGrow: 1 }}
+                variant="h4"
+                component="p"
+                color="initial"
               >
-                <MenuIcon />
-              </IconButton>
-            </Box>
-
-            <Box className="navAvatar">
-              <Tooltip title="Open settings">
-              <IconButton >
-                  <img
-                    crossOrigin="anonymous" 
-                    className="nav-img"
-                    src={
-                      
-                        `https://upskilling-egypt.com:3000/` +
-                        userData?.profileImage
-                    }
-                  />
-                </IconButton>
-              </Tooltip>
-            </Box>
-
-            <Box className="navInfo" sx={{marginLeft:'auto'}}>
-           
-              <Typography variant="caption" color="black" sx={{padding:'15px'}}>
-                {userData?.userName}
-              </Typography>
-              <Typography variant="caption" color="initial">
-                {userData?.email}
-              </Typography>
-            </Box>
-          </Toolbar>
-        </Container>
-                  </AppBar>*/}
-
-
-
-   </>
-  )
+                <Typography
+                  display={"inline"}
+                  style={{
+                    fontSize: "clamp(1.6rem, 4vw, 2rem)",
+                    color: "#3d5afe",
+                  }}
+                >
+                  Stay
+                </Typography>
+                cation.
+              </Typography>{" "}
+            </Grid>
+            <Grid  item xs={12} sm={6}>
+              <Box
+                display={"flex"}
+                alignItems={"center"}
+                justifyContent={{ xs: "center", sm: "end" }}
+              >
+                <Avatar
+                  src={
+                    userData?.profileImage == null
+                      ? defaultImage
+                      : userData?.profileImage
+                  }
+                />
+                <Typography
+                  variant="caption"
+                  color="black"
+                  sx={{ padding: "15px" }}
+                >
+                  {userData?.userName}
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
+        </AppBar>
+      </Box>
+    </>
+  );
 }
--- src/UsersModule/Components/Navbar/Navbar.tsx
+++ src/UsersModule/Components/Navbar/Navbar.tsx
@@ -1,151 +1,373 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import {   useNavigate } from "react-router-dom";
-import Link from '@mui/material/Link';
-import FavoriteIcon from '@mui/icons-material/Favorite';
+// import React from 'react';
+// import AppBar from '@mui/material/AppBar';
+// import Box from '@mui/material/Box';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import Divider from '@mui/material/Divider';
+// import Drawer from '@mui/material/Drawer';
+// import IconButton from '@mui/material/IconButton';
+// import List from '@mui/material/List';
+// import ListItem from '@mui/material/ListItem';
+// import ListItemButton from '@mui/material/ListItemButton';
+// import ListItemText from '@mui/material/ListItemText';
+// import MenuIcon from '@mui/icons-material/Menu';
+// import Toolbar from '@mui/material/Toolbar';
+// import Typography from '@mui/material/Typography';
+// import Button from '@mui/material/Button';
+// import {   useNavigate } from "react-router-dom";
+// import Link from '@mui/material/Link';
+// import FavoriteIcon from '@mui/icons-material/Favorite';
+// import { toast } from "react-toastify";
+
+
+// export default function Navbar() {
+//   let userRole=JSON.parse(localStorage.getItem('userRole')) ;
+//   console.log(userRole);
+
+//   let navigate=useNavigate();
+//   const navItems = ['Home', 'About', 'Contact'];
+//   const [mobileOpen, setMobileOpen] = React.useState(false);
+//   const handleDrawerToggle = () => {
+//     setMobileOpen((prevState) => !prevState);
+//   };
+
+//   <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+//   <Typography variant="h6" sx={{ my: 2 }}>
+//     MUI
+//   </Typography>
+//   <Divider />
+//   <List>
+//     {navItems.map((item) => (
+//       <ListItem key={item} disablePadding>
+//         <ListItemButton sx={{ textAlign: 'center' }}>
+//           <ListItemText primary={item} />
+//         </ListItemButton>
+//       </ListItem>
+//     ))}
+//   </List>
+// </Box>
+
+ 
+//   return (
+//    <>
+
+
+// <Box sx={{ flexGrow: 1 }}>
+//       <AppBar position="static" sx={{background:'#FFFFFF'}}>
+//         <Toolbar>
+         
+//         <Typography
+//            sx={{flexGrow:1}}
+//             variant="h4"
+//             component="div"
+//             color="initial"
+//           >
+//             <Typography
+//               variant=""
+             
+//               style={{ fontSize: "clamp(1rem, 4vw, 2rem)",color:'#3d5afe', marginLeft:'70px' }}
+//             >
+//               Stay
+//             </Typography>
+//             cation.
+//           </Typography>
+
+
+
+//           <Link href="/" sx={{margin:'10px',color:'black',textDecoration:'none',"&:hover":{color:'blue'}}}>
+//                  Home
+//           </Link>
+//           <Link href="/all-rooms" sx={{margin:'10px',color:'black',textDecoration:'none',"&:hover":{color:'blue'}}}>
+//                  Explore
+//           </Link>
+//           <IconButton sx={{p:'20px'}} >
+// {userRole=='user'?( <FavoriteIcon onClick={() => navigate("/favorite-room")} color="error"/>):(<FavoriteIcon onClick={()=>toast.error('sure login')}/>)}
+           
+//           </IconButton>
+        
+//           <Button variant="outlined">AR</Button>
+
+//          <Button variant="contained" color='primary'  sx={{ marginLeft: "10px",  height: "40px" }} onClick={() => navigate("./Authentication")}>Login Now</Button>
+//           <Button variant="contained" color='success' sx={{ marginLeft: "10px",  height: "40px" ,background:'#ffc107' }}  onClick={() => navigate("./Authentication/register")}>Sign Up</Button>
+         
+//         </Toolbar>
+//       </AppBar>
+//     </Box>
+//    {/*<Box sx={{ display: 'flex' }}>
+//       <CssBaseline />
+//       <AppBar component="nav" sx={{ color: "#152C5B", background: "#fff" }}>
+//         <Toolbar>
+//           <IconButton
+//             color="inherit"
+//             aria-label="open drawer"
+//             edge="start"
+//             onClick={handleDrawerToggle}
+//             sx={{ mr: 2, display: { sm: 'none' } }}
+//           >
+//             <MenuIcon />
+//           </IconButton>
+//           <Typography
+//             variant="h6"
+//             component="div"
+//             sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+//           >
+//             Staycation.
+//           </Typography>
+//           <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            
+//           <Typography
+//            variant="caption"
+//           >
+//           <Link href="/" underline="hover" sx={{fontSize:'25px',margin:'10px'}}> Home </Link>
+  
+//           </Typography>
+//           <Typography
+//             variant="caption"          >
+//            <Link href="/all-rooms" underline="hover" sx={{fontSize:'25px',margin:'10px'}}>Explore</Link>
+  
+ 
+
+//           </Typography>
+//           <Button variant="contained"  sx={{ marginLeft: "10px", width: "180px", height: "40px" }} onClick={() => navigate("./Authentication")}>Login</Button>
+//           <Button variant="caption"  sx={{ marginLeft: "10px", width: "180px", height: "40px" ,background:'#ffc107',color:'#FFFFFF' }}  onClick={() => navigate("./Authentication/register")}>SignUp</Button>
+         
+                   
+                  
+                  
+                 
+//           </Box>
+//         </Toolbar>
+//       </AppBar>
+//       <nav>
+       
+//       </nav>
+//       <Box component="main" sx={{ p: 3 }}>
+//         <Toolbar />
+        
+//       </Box>
+//   </Box>*/}
+    
+//    </>
+//   )
+// }
+
+
+
+
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Link } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
+interface Props {
+  window?: () => Window;
+}
 
-export default function Navbar() {
-  let userRole=JSON.parse(localStorage.getItem('userRole')) ;
-  console.log(userRole);
-
-  let navigate=useNavigate();
-  const navItems = ['Home', 'About', 'Contact'];
+const drawerWidth = 240;
+export default function DrawerAppBar(props: Props) {
+  const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
+  let userRole = JSON.parse(localStorage.getItem("userRole"));
+  let navigate = useNavigate();
+
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
   };
 
-  <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-  <Typography variant="h6" sx={{ my: 2 }}>
-    MUI
-  </Typography>
-  <Divider />
-  <List>
-    {navItems.map((item) => (
-      <ListItem key={item} disablePadding>
-        <ListItemButton sx={{ textAlign: 'center' }}>
-          <ListItemText primary={item} />
-        </ListItemButton>
-      </ListItem>
-    ))}
-  </List>
-</Box>
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
- 
   return (
-   <>
-
-
-<Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{background:'#FFFFFF'}}>
+    <Box sx={{ display: "flex",height:50 }}>
+      <CssBaseline />
+      <AppBar component="nav" sx={{ bgcolor: "#fff" }}>
         <Toolbar>
-         
-        <Typography
-           sx={{flexGrow:1}}
+          <Typography
+            sx={{ flexGrow: 1 }}
             variant="h4"
             component="div"
             color="initial"
           >
             <Typography
               variant=""
-             
-              style={{ fontSize: "clamp(1rem, 4vw, 2rem)",color:'#3d5afe', marginLeft:'70px' }}
+              style={{ fontSize: "clamp(1.6rem, 4vw, 2rem)", color: "#3d5afe" }}
             >
               Stay
             </Typography>
             cation.
           </Typography>
 
+          <Box
+            sx={{
+              display: {
+                xs: "none",
+                sm: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: 2,
+              },
+            }}
+          >
+            <Link
+              href="/"
+              mx={"5px"}
+              sx={{
+                color: "black",
+                textDecoration: "none",
+                "&:hover": { color: "blue" },
+              }}
+            >
+              Home
+            </Link>
+            <Link
+              href="/all-rooms"
+              mx={"5px"}
+              sx={{
+                color: "black",
+                textDecoration: "none",
+                "&:hover": { color: "blue" },
+              }}
+            >
+              Explore
+            </Link>
+            <IconButton sx={{ height: "40px", width: "40px" }}>
+              {userRole == "user" ? (
+                <FavoriteIcon
+                  onClick={() => navigate("/favorite-room")}
+                  color="error"
+                />
+              ) : (
+                <FavoriteIcon onClick={() => toast.error("sure login")} />
+              )}
+            </IconButton>
 
+            <Button variant="outlined">AR</Button>
 
-          <Link href="/" sx={{margin:'10px',color:'black',textDecoration:'none',"&:hover":{color:'blue'}}}>
-                 Home
-          </Link>
-          <Link href="/all-rooms" sx={{margin:'10px',color:'black',textDecoration:'none',"&:hover":{color:'blue'}}}>
-                 Explore
-          </Link>
-          <IconButton sx={{p:'20px'}} >
-{userRole=='user'?( <FavoriteIcon onClick={() => navigate("/favorite-room")} color="error"/>):(<FavoriteIcon onClick={()=>toast.error('sure login')}/>)}
-           
-          </IconButton>
-        
-          <Button variant="outlined">AR</Button>
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{ height: "40px", lineHeight: "1" }}
+              onClick={() => navigate("./Authentication")}
+            >
+              Login Now
+            </Button>
+            <Button
+              variant="contained"
+              color="success"
+              sx={{ height: "40px", lineHeight: "1", background: "#ffc107" }}
+              onClick={() => navigate("./Authentication/register")}
+            >
+              Sign Up
+            </Button>
+          </Box>
 
-         <Button variant="contained" color='primary'  sx={{ marginLeft: "10px",  height: "40px" }} onClick={() => navigate("./Authentication")}>Login Now</Button>
-          <Button variant="contained" color='success' sx={{ marginLeft: "10px",  height: "40px" ,background:'#ffc107' }}  onClick={() => navigate("./Authentication/register")}>Sign Up</Button>
-         
-        </Toolbar>
-      </AppBar>
-    </Box>
-   {/*<Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar component="nav" sx={{ color: "#152C5B", background: "#fff" }}>
-        <Toolbar>
           <IconButton
             color="inherit"
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+            sx={{
+              display: { xs: "flex", sm: "none" },
+              justifyContent: "end",
+            }}
           >
-            <MenuIcon />
+            <MenuIcon sx={{ color: "#3d5afe" }} />
           </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-            Staycation.
-          </Typography>
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-            
-          <Typography
-           variant="caption"
-          >
-          <Link href="/" underline="hover" sx={{fontSize:'25px',margin:'10px'}}> Home </Link>
-  
-          </Typography>
-          <Typography
-            variant="caption"          >
-           <Link href="/all-rooms" underline="hover" sx={{fontSize:'25px',margin:'10px'}}>Explore</Link>
-  
- 
-
-          </Typography>
-          <Button variant="contained"  sx={{ marginLeft: "10px", width: "180px", height: "40px" }} onClick={() => navigate("./Authentication")}>Login</Button>
-          <Button variant="caption"  sx={{ marginLeft: "10px", width: "180px", height: "40px" ,background:'#ffc107',color:'#FFFFFF' }}  onClick={() => navigate("./Authentication/register")}>SignUp</Button>
-         
-                   
-                  
-                  
-                 
-          </Box>
         </Toolbar>
       </AppBar>
       <nav>
-       
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          <Box
+            p={4}
+            gap={3}
+            display={"flex"}
+            flexDirection={"column"}
+            alignItems={"center"}
+            justifyContent={"center"}
+          >
+            <Link
+              href="/"
+              display={"block"}
+              sx={{
+                textAlign: "center",
+                color: "black",
+                textDecoration: "none",
+                "&:hover": { color: "blue" },
+              }}
+            >
+              Home
+            </Link>
+            <Link
+              href="/all-rooms"
+              display={"block"}
+              sx={{
+                textAlign: "center",
+                color: "black",
+                textDecoration: "none",
+                "&:hover": { color: "blue" },
+              }}
+            >
+              Explore
+            </Link>
+            <IconButton sx={{ height: "50px", width: "50px" }}>
+              {userRole == "user" ? (
+                <FavoriteIcon
+                  onClick={() => navigate("/favorite-room")}
+                  color="error"
+                />
+              ) : (
+                <FavoriteIcon onClick={() => toast.error("sure login")} />
+              )}
+            </IconButton>
+
+            <Button variant="outlined">AR</Button>
+
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{ height: "40px", display: "block" }}
+              onClick={() => navigate("./Authentication")}
+            >
+              Login Now
+            </Button>
+            <Button
+              variant="contained"
+              color="success"
+              sx={{ height: "40px", background: "#ffc107", display: "block" }}
+              onClick={() => navigate("./Authentication/register")}
+            >
+              Sign Up
+            </Button>
+          </Box>
+        </Drawer>
       </nav>
       <Box component="main" sx={{ p: 3 }}>
         <Toolbar />
-        
       </Box>
-  </Box>*/}
-    
-   </>
-  )
+    </Box>
+  );
 }
